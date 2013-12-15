04ed8b5580f424018bf4c03a8d8c127a0915a4cf
Tim Poisot <timothee.poisot@uqar.ca>
2013-12-15 12:49:46 -0500

    Functions and classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 using namespace std;
 
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+#include <math.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_randhist.h>
+#include <gsl/gsl_sf_exp.h>
+#include <gsl/gsl_sf_log.h>
+
 // Type 2 functional response
 // a: attack rate
 // h: handling time
@@ -11,3 +22,24 @@ double TypeII(double a, double h, double N)
    return Numerator/Denominator;
 }
 
+// Class SPECIES
+class SPECIES
+{
+   public:
+      double N;
+      double x;
+      double dN;
+      double nmut;
+      double birth;
+}
+
+// Gaussian with constant area under curve
+// x:  Position 1
+// y:  Position 2
+// xi: Breadth
+double Gaussian(double x, double y, double xi)
+{
+   const double mismatch = ((x - y) * (x - y)) / xi;
+   const double correct  = 1.0 / (xi * sqrt(M_PI * 2.0));
+   return correct * gsl_sf_exp(-0.5*(mismatch * mismatch));
+}
