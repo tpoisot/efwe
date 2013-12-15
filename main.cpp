9d2e11e956ffb969b94925343d77bf3dc9374d27
Tim Poisot <timothee.poisot@uqar.ca>
2013-12-15 15:52:47 -0500

    Extinction of populations with N lower than RARITY

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 using namespace std;
 
 #include <vector>
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <cmath>
@@ -10,6 +11,7 @@ using namespace std;
 #include <gsl/gsl_randist.h>
 #include <gsl/gsl_sf_exp.h>
 #include <gsl/gsl_sf_log.h>
+
 
 // Type 2 functional response
 // a: attack rate
@@ -31,6 +33,7 @@ class SPECIES
       double dN;
       int nmut;
       int birth;
+      bool extinct;
 };
 
 // Class PARAMS
@@ -50,8 +53,21 @@ class PARAMS
       unsigned SIM_STEPS;
       unsigned SUB_STEPS;
       unsigned REC_EACH;
+      double RARITY;
 };
 
+// Functions for extinctions
+bool toDelete(const SPECIES & S)
+{
+   return S.extinct;
+}
+
+void extinct(vector<SPECIES>* S, PARAMS* P)
+{
+   (*S).erase(
+         remove_if((*S).begin(), (*S).end(), toDelete),
+         (*S).end());
+}
 // Gaussian with constant area under curve
 // x:  Position 1
 // y:  Position 2
@@ -70,36 +86,34 @@ int main(int argc, char *argv[])
    gsl_rng *rng = gsl_rng_alloc(gsl_rng_taus2);
    gsl_rng_set(rng, begin_time);
    // Default options
-   PARAMS P = {0.5, 0.1, 0.1, 1.01, 1.1, 0.15, 0.90, 0.07, 100, 100, 10, 50, 10};
+   PARAMS P = {0.5, 0.1, 0.1, 1.01, 1.1, 0.15, 0.90, 0.07, 100, 100, 10, 50, 10, 0.01};
    // TODO get options
    // TODO write options as JSON
    // Initialize list of species
    vector<SPECIES> PREYS;
-   PREYS.push_back({10.0, 0.0, 0.0, 0, 0});
+   PREYS.push_back({10.0, 0.0, 0.0, 0, 0, false});
    vector<SPECIES> PREDS;
-   PREDS.push_back({3.0, 0.0, 0.0, 0, 0});
+   PREDS.push_back({3.0, 0.0, 0.0, 0, 0, false});
    // TODO population dynamics loop
    unsigned mainStep = 0;
    for(mainStep = 0 ; mainStep < P.SIM_STEPS ; ++mainStep)
    {
+      unsigned pred_it, prey_it, second_prey_it;
       // Enter sub-loop
       unsigned subStep = 0;
       for(subStep = 0 ; subStep < P.SUB_STEPS ; ++subStep)
       {
          // Loop through the PREDS
-         unsigned pred_it;
          for(pred_it = 0 ; pred_it < PREDS.size() ; ++pred_it)
          {
             PREDS[pred_it].dN -= P.d0 * PREDS[pred_it].N;
          }
          // Loop through the PREYS
-         unsigned prey_it;
          for(prey_it = 0 ; prey_it < PREYS.size() ; ++prey_it)
          {
             // Growth rate
             PREYS[prey_it].dN += P.r0 * Gaussian(0.0, PREYS[prey_it].x, P.breadth_grow);
             // Competition
-            unsigned second_prey_it;
             for(second_prey_it = 0 ; second_prey_it < PREYS.size() ; ++second_prey_it)
             {
                PREYS[prey_it].dN -= (PREYS[second_prey_it].N * Gaussian(PREYS[prey_it].x, PREYS[second_prey_it].x, P.breadth_comp) * P.q0);
@@ -116,17 +130,34 @@ int main(int argc, char *argv[])
          for(prey_it = 0 ; prey_it < PREYS.size() ; ++prey_it)
          {
             PREYS[prey_it].N += ((PREYS[prey_it].N * PREYS[prey_it].dN) * (1.0 / P.SUB_STEPS));
-            PREYS[prey_it].dN = 0.0;
+            if(PREYS[prey_it].N < P.RARITY)
+            {
+               PREYS[prey_it].extinct = true;
+            }
          }
          // Updated population sizes for predators
          for(pred_it = 0 ; pred_it < PREDS.size() ; ++pred_it)
          {
             PREDS[pred_it].N += ((PREDS[pred_it].N * PREDS[pred_it].dN) * (1.0 / P.SUB_STEPS));
-            PREDS[pred_it].dN = 0.0;
+            if(PREDS[pred_it].N < P.RARITY)
+            {
+               PREDS[pred_it].extinct = true;
+            }
          }
       }
       // TODO mutations
       // TODO extinctions
+      extinct(&PREYS, &P);
+      extinct(&PREDS, &P);
+      // Set derivatives to 0
+      for(prey_it = 0 ; prey_it < PREYS.size() ; ++prey_it)
+      {
+         PREYS[prey_it].dN = 0.0;
+      }
+      for(pred_it = 0 ; pred_it < PREDS.size() ; ++pred_it)
+      {
+         PREDS[pred_it].dN = 0.0;
+      }
       // TODO record
    }
 }
